bebccc9384fa17802fa7238f5318cef84f3ff020
Kaushal K <kaushal.k.1241@gmail.com>
2016-01-10 12:31:30 -0500

    fix typo in teamMatches domain

--- backend/db/lib/domain/teamsMatches.js
+++ backend/db/lib/domain/teamsMatches.js
@@ -1,5 +1,5 @@
 module.exports = function (dbContext, dbDomain) {
 	'use strict';
 
-	return require('./db-record')(dbContext.models.teamsMatches, dbContext);
+	return require('./db-record')(dbContext.models.teamMatches, dbContext);
 };
