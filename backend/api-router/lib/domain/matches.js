5bf4cd23d4a6e79ec45a29f3c861ee277ff5e85c
Kaushal K <kaushal.k.1241@gmail.com>
2016-01-10 11:44:49 -0500

    updated schema for team match recording

--- backend/api-router/lib/domain/matches.js
+++ backend/api-router/lib/domain/matches.js
@@ -17,27 +17,58 @@ module.exports = function (db, apiDomain) {
 				});
 			})
 			.then(function(match) {
-				return c.domain.matches.upsert({
-					id: match.id,
-					event_code: eventCode,
-					match_number: details.matchNumber,
-					red_score: details.redFinal || match.red_score,
-					red_penalties: details.redPenalties || match.red_penalties,
-					blue_score: details.blueFinal || match.blue_score,
-					blue_penalties: details.bluePenalties || match.blue_penalties
-				});
+				
 			})
 			.then(function(createdMatch) {
+                var autoTotal = (details.autoHigh + details.autoBreach) * 10 + details.autoLow * 5 + details.autoReach * 2,
+                    teleTotal = (details.crossTotal + details.teleHigh) * 5 + details.teleLow * 2,
+                    endTotal = details.scaled * 15 + details.challenged * 5;
+
 				c.domain.teamsMatches.create({
-					dq: details.dq || false,
 					match_uid: createdMatch.id,
 					team_number: details.teamNumber,
-					penalties: details.penalties || 0,
-					auto_points: details.autoPoints || 0,
-					tele_points: details.telePoints || 0,
-					total_points: details.totalPoints || 0,
-					endgame_pts: details.endgamePoints || 0
-				});
+					match_number: createdMatch.matchNumber,
+                    
+                    auto_high: details.autoHigh || 0,
+			        auto_low: details.autoLow || 0,
+			        auto_points: autoTotal || 0,
+		            outer_works_breach: details.autoBreach || 0,
+			        outer_works_category: details.autoBreachCategory || '',
+			        outer_works_reach: details.autoReach || 0,
+                    /* Tele Op Columns */
+                    boulder_pickup: details.telePickup || 0,
+                    scoring_angle: details.scoringAngle || 'none',
+                    scoring_range: details.scoringRange || 'none',
+                    tele_high: details.teleHigh || 0,
+                    tele_low: details.teleLow || 0,
+                    /* Tele Op Outerworks Columns */
+                    outerworks_A: details.categoryA || false,
+                    outerworks_A_count: details.categoryA ? details.categoryACount : 0,
+                    outerworks_B: details.categoryB || false,
+                    outerworks_B_count: details.categoryB ? details.categoryBCount : 0,
+                    outerworks_C: details.categoryC || false,
+                    outerworks_C_count: details.categoryC ? details.categoryCCount : 0,
+                    outerworks_D: details.categoryD || false,
+                    outerworks_D_count: details.categoryD ? details.categoryDCount : 0,
+                    outerworks_E: details.categoryE || false,
+                    outerworks_E_count: details.categoryE ? details.categoryECount : 0,
+                    /* Tele Op Score Columns */
+                    tele_points: teleTotal,
+                    /* End Game Columns */
+                    challenge: details.challenged || 0,
+                    scale: details. scaled|| 0,
+                    endgame_pts: endTotal,
+                    /* Penalty Columns */
+                    dq: details.dq || false,
+                    fouls: details.fouls || 0,
+                    tech_fouls: details.techFouls || 0,
+                    /* End Match Columns */
+                    bonus_breach: details.breached || 0,
+                    bonus_capture: details.captured || 0,
+                    comments: details.comments || '',
+                    result: details.result || 'loss',
+                    total_points: autoTotal + teleTotal + endTotal
+                });
 			})
 			.then(function(createdMatch) {
 				deferred.resolve(createdMatch);
--- backend/db/lib/models/teamsMatches.js
+++ backend/db/lib/models/teamsMatches.js
@@ -3,13 +3,48 @@ module.exports = function(dbServer) {
 
 	return dbServer.define('teams_matches',
 		dbServer.conventions.newFieldSet([
-			{ name: 'match_number', type: 'integer', allowNull: false },
+			/* General Match Columns */
+            { name: 'match_number', type: 'integer', allowNull: false },
+            /* Auto Columns */
+			{ name: 'auto_high', type: 'boolean', allowNull: false },
+			{ name: 'auto_low', type: 'boolean', allowNull: false },
 			{ name: 'auto_points', type: 'integer', allowNull: false },
-			{ name: 'tele_points', type: 'integer', allowNull: false },
+			{ name: 'outer_works_breach', type: 'boolean', allowNull: false },
+			{ name: 'outer_works_category', type: 'string', allowNull: false },
+			{ name: 'outer_works_reach', type: 'boolean', allowNull: false },
+            /* Tele Op Columns */
+			{ name: 'boulder_pickup', type: 'integer', allowNull: false },
+			{ name: 'scoring_angle', type: 'string', allowNull: false },
+			{ name: 'scoring_range', type: 'string', allowNull: false },
+			{ name: 'tele_high', type: 'integer', allowNull: false },
+			{ name: 'tele_low', type: 'integer', allowNull: false },
+            /* Tele Op Outerworks Columns */
+			{ name: 'outerworks_A', type: 'boolean', allowNull: false },
+			{ name: 'outerworks_A_count', type: 'integer', allowNull: false },
+			{ name: 'outerworks_B', type: 'boolean', allowNull: false },
+			{ name: 'outerworks_B_count', type: 'integer', allowNull: false },
+			{ name: 'outerworks_C', type: 'boolean', allowNull: false },
+			{ name: 'outerworks_C_count', type: 'integer', allowNull: false },
+			{ name: 'outerworks_D', type: 'boolean', allowNull: false },
+			{ name: 'outerworks_D_count', type: 'integer', allowNull: false },
+			{ name: 'outerworks_E', type: 'boolean', allowNull: false },
+			{ name: 'outerworks_E_count', type: 'integer', allowNull: false },
+			/* Tele Op Score Columns */
+            { name: 'tele_points', type: 'integer', allowNull: false },
+			/* End Game Columns */
+            { name: 'challenge', type: 'boolean', allowNull: false },
+			{ name: 'scale', type: 'boolean', allowNull: false },
 			{ name: 'endgame_pts', type: 'integer', allowNull: false },
-			{ name: 'total_points', type: 'integer', allowNull: false },
-			{ name: 'penalties', type: 'integer', allowNull: false },
-			{ name: 'dq', type: 'boolean', allowNull: false }
+            /* Penalty Columns */
+			{ name: 'dq', type: 'boolean', allowNull: false },
+            { name: 'fouls', type: 'integer', allowNull: false },
+			{ name: 'tech_fouls', type: 'integer', allowNull: false },
+            /* End Match Columns */
+			{ name: 'bonus_breach', type: 'boolean', allowNull: false },
+			{ name: 'bonus_capture', type: 'boolean', allowNull: false },
+			{ name: 'comments', type: 'string' },
+			{ name: 'result', type: 'string', allowNull: false },
+			{ name: 'total_points', type: 'integer', allowNull: false }
 		]),
 		dbServer.conventions.newTable('teams_matches')
 	);
